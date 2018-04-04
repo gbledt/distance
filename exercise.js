82a8d78ece04834b8debc3a57723540456bd010b
gbledt <gbledt@mit.edu>
2018-04-04 10:31:14 -0400

    Class exercise

--- exercise.js
+++ exercise.js
@@ -5,7 +5,9 @@ exercise.distance = function(p1,p2){
     //-------------------
     //---- Your Code ----
     //-------------------    
-    return 'Error: 1st function not implemented';    
+    var dx = p2.x - p1.x;
+    var dy = p2.y - p1.y;
+    return Math.sqrt(dx*dx + dy*dy);    
 };
 
 // calculate distance between new point and existing
@@ -13,7 +15,12 @@ exercise.distances = function(newPoint,data){
     //-------------------
     //---- Your Code ----
     //-------------------
-    return 'Error: 2nd function not implemented';
+    data.forEach(function(existingPoint){
+		var dx = existingPoint.x - newPoint.x;
+		var dy = existingPoint.y - newPoint.y;
+		existingPoint.distance = Math.sqrt(dx*dx + dy*dy);
+	});
+    return data;
 };
 
 // find k closest points
@@ -21,7 +28,10 @@ exercise.findClosest = function(k,data){
     //-------------------
     //---- Your Code ----
     //------------------- 
-    return 'Error: 3rd function not implemented';    
+    data.sort(function(a,b){
+		return a.distance - b.distance;
+	});
+    return data.slice(0,k);    
 };
 
 module.exports = exercise;
